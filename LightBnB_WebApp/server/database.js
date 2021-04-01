5302ea868cbfb845a8880e7efb99865fa8ba48e4
Karina Song <ksysong@gmail.com>
2021-04-01 22:54:20 +0000

    add filter option for min price per night

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -116,10 +116,18 @@ const getAllProperties = function(options, limit = 10) {
     }
   }
 
-  // if (options.minimum_price_per_night && options.maximum_price_per_night) {
+  if (options.minimum_price_per_night) {
+    queryParams.push(`%${options.minimum_price_per_night}%`);
+    if (queryParams.length === 1) {
+      queryString += `WHERE cost_per_night $${queryParams.length} `;
+    } else {
+      queryString += `AND cost_per_night $${queryParams.length} `;
+    }
+  }
+
+  // if (options.maximum_price_per_night) {
 
   // }
-
   // if (options.minimum_rating) {
 
   // }
